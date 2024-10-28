2c468797a00fecd07842c2d0d9b20adc974a9df7
Gavin K <dgavink12@gmail.com>
2024-10-28 16:50:20 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,45 @@
+package sortingapp;
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        bubbleSortDescending(arr);
+
+        System.out.println("Sorted array in descending order:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void bubbleSortDescending(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+}
